366f39d92d04dac6d2efa2575b77f290e4e62188
M.ngugi <mngugi7@gmail.com>
2023-09-21 00:54:29 +0300

    goTwo.go

---
+++ goTwo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declare and initialize a string variable named "fruits" with a list of fruit names.
+	var fruits string = "Banana, Lemon, Passion, Pineapple, Orange"
+
+	// Declare and initialize a string variable named "cereals" with a list of cereal names.
+	var cereals string = "rice, wheat, rye, oats, barley, millet, and maize"
+
+	// Declare and initialize an integer variable named "j" with the value 10.
+	j := 10
+
+	// Print the value of the "fruits" variable to the console.
+	fmt.Println(fruits)
+
+	// Print the value of the "cereals" variable to the console.
+	fmt.Println(cereals)
+
+	// Print the value of the "j" variable to the console.
+	fmt.Println(j)
+}
