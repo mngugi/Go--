4a6819f615f3493fe0406a8f07e07c98fed18aa5
M.ngugi <mngugi7@gmail.com>
2023-09-20 12:56:16 +0300

    Update goOne.go

--- goOne.go
+++ goOne.go
@@ -1,8 +1,16 @@
+// This is the main package, which is the entry point of the Go program.
 package main
-import ("fmt")
 
+// Import the "fmt" package, which provides functions for formatted I/O.
+import (
+	"fmt"
+)
+
+// The main function is the entry point of the program.
 func main() {
-  fmt.Println("Hello World!")
-  fmt.Println("Lets Go its going to be fun!!")
-}
+	// Print the message "Hello World!" to the standard output (console).
+	fmt.Println("Hello World!")
 
+	// Print the message "Lets Go its going to be fun!!" to the standard output.
+	fmt.Println("Lets Go its going to be fun!!")
+}
